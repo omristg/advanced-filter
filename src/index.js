399e7cd07572b4f33af270e7266d9bc30afdcdbc
Omri Steinberg <omristg@gmail.com>
2022-03-21 22:57:10 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { RootCmp } from './RootCmp';
+import './assets/style/styles.scss'
+
+ReactDOM.render(
+	<React.StrictMode>
+		<RootCmp />
+	</React.StrictMode>,
+	document.getElementById('root')
+);
+
